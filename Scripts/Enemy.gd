19a709e42ce17c0231d8cfeacb149faf3d044298
AlexandreOkita <37855969+AlexandreOkita@users.noreply.github.com>
2018-04-14 13:48:54 -0300

    Add files via upload

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
 func _ready():
-	add_to_group('enemy')
-	connect('body_entered', self, '_on_area_enter')
+	get_node("vision").add_to_group('enemy')
+	connect('area_enter', self, '_on_area_enter')
 
 func _on_area_enter(other):
 	get_tree().change_scene("res://Cenas/gameover.tscn")
