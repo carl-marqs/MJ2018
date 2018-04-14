d2c9ed87c5b093d7a5ca6e3094748899aa3863e6
Carlos Marques <carlos_marques@email.com>
2018-04-14 11:50:13 -0300

    Update menu.gd

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -3,8 +3,8 @@ extends Node2D
 
 
 func _on_Button_pressed():
-	get_tree().change_scene("res://cenas/Stage_1.tscn")
+	get_tree().change_scene("res://Cenas/Stage_1.tscn")
 
 func _on_Button1_pressed():
-	get_tree().change_scene("res://cenas/menucreditos.tscn")
+	get_tree().change_scene("res://Cenas/menucreditos.tscn")
 	
