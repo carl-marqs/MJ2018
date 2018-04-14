5e6f75ff4b73a2931ebb770354d0def5c540f7f5
Carlos Marques <carlos_marques@email.com>
2018-04-14 10:13:15 -0300

    Update Coin.gd

--- Scripts/Coin.gd
+++ Scripts/Coin.gd
@@ -5,4 +5,10 @@ func _ready():
 
 func _on_body_enter(other):
 	if other.is_in_group('player'):
+		Singleton.dinheiro += 1
+		
+		var root = get_tree().get_root()
+		var root2 = root.get_child( root.get_child_count()-1 )
+		root2.find_node('dinheiro').dinheiro = Singleton.dinheiro
+		
 		queue_free()
