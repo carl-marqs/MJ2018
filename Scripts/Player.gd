e05032b66a134a56f0e2b31176d540c780ed194c
Carlos Marques <carlos_marques@email.com>
2018-04-14 10:14:12 -0300

    Update Player.gd

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-export var MOTION_SPEED = 200
+export var MOTION_SPEED = 120
 
 var rayNode
 
@@ -15,15 +15,30 @@ func _process(delta):
 	if Input.is_key_pressed(KEY_W) or Input.is_action_pressed('ui_up'):
 		motion += Vector2(0, -1)
 		rayNode.set_rotd(180)
+		get_node('sprite').set_rotd(180)
+		get_node('sprite').play('moving')
+		get_node('collision').set_rotd(180-90)
 	if Input.is_key_pressed(KEY_D) or Input.is_action_pressed('ui_right'):
 		motion += Vector2(1, 0)
 		rayNode.set_rotd(90)
+		get_node('sprite').set_rotd(90)
+		get_node('sprite').play('moving')
+		get_node('collision').set_rotd(90-90)
 	if Input.is_key_pressed(KEY_S) or Input.is_action_pressed('ui_down'):
 		motion += Vector2(0, 1)
 		rayNode.set_rotd(0)
+		get_node('sprite').set_rotd(0)
+		get_node('sprite').play('moving')
+		get_node('collision').set_rotd(0-90)
 	if Input.is_key_pressed(KEY_A) or Input.is_action_pressed('ui_left'):
 		motion += Vector2(-1, 0)
 		rayNode.set_rotd(270)
+		get_node('sprite').set_rotd(270)
+		get_node('sprite').play('moving')
+		get_node('collision').set_rotd(270-90)
+		
+	if !Input.is_key_pressed(KEY_W) and !Input.is_key_pressed(KEY_D) and !Input.is_key_pressed(KEY_S) and !Input.is_key_pressed(KEY_A) and !Input.is_action_pressed('ui_up') and !Input.is_action_pressed('ui_right') and !Input.is_action_pressed('ui_down') and !Input.is_action_pressed('ui_left'):
+		get_node('sprite').play('idle')
 		
 	motion = motion.normalized() * MOTION_SPEED * delta
 	move(motion)
