cb379f5e4f342adc87fbec40a9bf999e34830f60
Carlos Marques <carlos_marques@email.com>
2018-04-14 00:14:38 -0300

    Update Player.gd

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,26 +1,26 @@
 extends KinematicBody2D
 
-export var MOTION_SPEED = 140
+export var MOTION_SPEED = 200
 
 var rayNode
 
 func _ready():
-	set_physics_process(true)
+	set_process(true)
 	rayNode = get_node('ray')
 
-func _physics_process(delta):
+func _process(delta):
 	var motion = Vector2()
 	
-	if Input.is_action_pressed('ui_up'):
+	if Input.is_key_pressed(KEY_W) or Input.is_action_pressed('ui_up'):
 		motion += Vector2(0, -1)
 		rayNode.set_rotd(180)
-	if Input.is_action_pressed('ui_right'):
+	if Input.is_key_pressed(KEY_D) or Input.is_action_pressed('ui_right'):
 		motion += Vector2(1, 0)
 		rayNode.set_rotd(90)
-	if Input.is_action_pressed('ui_down'):
+	if Input.is_key_pressed(KEY_S) or Input.is_action_pressed('ui_down'):
 		motion += Vector2(0, 1)
 		rayNode.set_rotd(0)
-	if Input.is_action_pressed('ui_left'):
+	if Input.is_key_pressed(KEY_A) or Input.is_action_pressed('ui_left'):
 		motion += Vector2(-1, 0)
 		rayNode.set_rotd(270)
 		
