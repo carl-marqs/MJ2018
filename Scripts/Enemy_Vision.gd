f86bd582e076801a6e75016dd2e9d36cedfd7bee
Carlos Marques <carlos_marques@email.com>
2018-04-14 11:55:51 -0300

    Update Enemy_Vision.gd

--- Scripts/Enemy_Vision.gd
+++ Scripts/Enemy_Vision.gd
@@ -4,5 +4,5 @@ func _ready():
 	connect('area_enter', self, '_on_area_enter')
 
 func _on_area_enter(other):
-	if other.is_in_group('enemy')
+	if other.is_in_group('enemy'):
 		get_tree().change_scene("res://Cenas/gameover.tscn")
